ca8d742c811ad3d04f3cdbad8df573032a4a8566
Lucy Toman <lucy.toman@thinslices.com>
2025-01-03 18:19:17 +0100

    adds useActionState hook to About form

--- src/About.tsx
+++ src/About.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react';
+import { useActionState } from 'react';
 import { updateNameInDB } from './api';
 
 const About = () => {
-  const [name, setName] = useState(
-    localStorage.getItem('fullname') || 'Anonymous user'
+  const [name, actionFunction, isPending] = useActionState(
+    formAction,
+    localStorage.getItem('name') || 'Anonymous user'
   );
 
-  async function formAction(formData: FormData) {
+  async function formAction(prevState: string | undefined, formData: FormData) {
     try {
       const firstname = formData.get('firstname');
       const lastname = formData.get('lastname');
       const name = `${firstname} ${lastname}`;
       if (name !== null) {
         const newName = await updateNameInDB(name as string);
-        setName(newName);
+        return newName;
       }
     } catch (error) {
       console.error(error);
@@ -25,7 +26,7 @@ const About = () => {
       <h2 className='username'>
         Current user: <span id='user-name'>{name}</span>
       </h2>
-      <form action={formAction}>
+      <form action={actionFunction}>
         <section className='form-section'>
           <label htmlFor='name1'>Name:</label>
           <input type='text' id='name1' name='firstname' required />
